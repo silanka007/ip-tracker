62dbf1c538a7b3dd8c0b68db9035bdc12910202c
Paul Onyekwelu <silanka@CEO-MacBook-Pro.local>
2020-10-09 20:42:56 +0100

    importation of rubik font as applications primary font-family

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import AddressInfo from './components/AddressInfo';
 import Header from './components/Header';
+import MapSection from './components/MapSection';
 
 function App() {
   return (
     <div className="App">
       <Header />
       <AddressInfo />
+      <MapSection />
     </div>
   );
 }
---
+++ src/components/MapSection.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const MapSection = () => {
+    return (
+        <div className="map-section"></div>
+    )
+}
+
+export default MapSection;
